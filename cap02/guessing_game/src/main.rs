23ed39e4ee99e3fd1ac4f4fa47fe8d5e4e9fe0cc
Daniel Nunes <danielmnunes08@gmail.com>
2023-11-18 09:02:07 -0300

    capitulo 02

---
+++ cap02/guessing_game/src/main.rs
@@ -0,0 +1,35 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
+fn main() {
+    println!("Adivinhe o número");
+
+    let numero_secreto = rand::thread_rng().gen_range(1..=100);
+    // println!("O número secreto é: {numero_secreto}");
+
+    loop {
+        println!("Digite seu palpite");
+        let mut palpite = String::new();
+
+        io::stdin()
+            .read_line(&mut palpite)
+            .expect("falha ao ler entrada");
+
+        let palpite: u32 = match palpite.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("Vc disse: {palpite}");
+
+        match palpite.cmp(&numero_secreto) {
+            Ordering::Less => println!("Muito baixo!"),
+            Ordering::Greater => println!("Muito alto!"),
+            Ordering::Equal => {
+                println!("Você acertou!");
+                break;
+            }
+        }
+    }
+}
