24a7ab6f851b7e22ce66ea032e1c28e826e3ef43
Daniel Nunes <danielmnunes08@gmail.com>
2023-11-18 23:18:02 -0300

    Operações

--- cap02/variables/src/main.rs
+++ cap02/variables/src/main.rs
@@ -1,6 +1,9 @@
+#[allow(unused_variables)]
 fn main() {
-    // tipos de ponto flutuante
-    let x = 2.0;
-    let y: f32 = 3.1;
-    println!("{x} e {y}");
+    // Operações numéricas
+    let soma = 5 + 10;
+    let diferenca = 4 - 5;
+    let produto = 3 * 9;
+    let quociente = 3 / 1;
+    let resto = 9 % 4;
 }
