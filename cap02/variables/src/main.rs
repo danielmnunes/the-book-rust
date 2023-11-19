b4c615909d7542a32949dbba46d72de29f9d72fd
Daniel Nunes <danielmnunes08@gmail.com>
2023-11-18 23:23:53 -0300

    O tipo booleano

--- cap02/variables/src/main.rs
+++ cap02/variables/src/main.rs
@@ -1,9 +1,6 @@
 #[allow(unused_variables)]
 fn main() {
-    // Operações numéricas
-    let soma = 5 + 10;
-    let diferenca = 4 - 5;
-    let produto = 3 * 9;
-    let quociente = 3 / 1;
-    let resto = 9 % 4;
+    // O tipo booleano
+    let t = true;
+    let f: bool = false;
 }
