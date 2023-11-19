9fccfd5d32f11ec9228b195f0ca09ef662b43467
Daniel Nunes <danielmnunes08@gmail.com>
2023-11-18 23:10:22 -0300

    definindo variavel com tipo

--- cap02/variables/src/main.rs
+++ cap02/variables/src/main.rs
@@ -1,15 +1,4 @@
 fn main() {
-    // Definição com let
-    let x = 5;
-
-    // Shadowing
-    let x = x + 1;
-    let x = x * 2;
-
-    println!("valor de x é: {x}");
-
-    let espacos = "   ";
-    // espacos = espacos.len(); // não funciona'
-    let espacos = espacos.len();
-    println!("espaços: {espacos}")
+    let guess: u32 = "42".parse().expect("Não é um número");
+    println!("{guess}");
 }
