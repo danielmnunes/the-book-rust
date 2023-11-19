ae252af59242255863643b35eb3a4f54e68ec1ff
Daniel Nunes <danielmnunes08@gmail.com>
2023-11-18 23:25:35 -0300

    O tipo de caractere

--- cap02/variables/src/main.rs
+++ cap02/variables/src/main.rs
@@ -1,6 +1,8 @@
 #[allow(unused_variables)]
 fn main() {
-    // O tipo booleano
-    let t = true;
-    let f: bool = false;
+    // O tipo de caractere
+    let c = 'z';
+    let h = '👏';
+
+    println!("{h}");
 }
