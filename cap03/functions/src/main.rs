edf0e6cbd0d32b4dadae9c59f9cf048e42235bbe
Daniel Nunes <danielmnunes08@gmail.com>
2023-11-24 10:48:13 -0300

    funções

--- cap03/functions/src/main.rs
+++ cap03/functions/src/main.rs
@@ -1,3 +1,24 @@
 fn main() {
     println!("Hello, world!");
+    outra_funcao(12, 13);
+
+
+    let y = {
+        let x = 3;
+        println!("{x}");
+        x * 11
+    };
+    println!("{y}");
+
+    let x = cinco();
+    println!("{x}");
 }
+
+fn outra_funcao(x: i32, y: i32) {
+    println!("Valor de x: {x}-{y}");
+}
+
+fn cinco() -> i32 {
+    5
+}
+
