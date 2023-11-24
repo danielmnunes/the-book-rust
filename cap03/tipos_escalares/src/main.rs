4c1f8ce621160b6ee1bed0d08a5648c79097af2a
Daniel Nunes <danielmnunes08@gmail.com>
2023-11-23 23:41:26 -0300

    tipos escalares

---
+++ cap03/tipos_escalares/src/main.rs
@@ -0,0 +1,28 @@
+#[allow(unused_variables)]
+fn main() {
+    // inteiros
+    let i1 = 1_000;
+    let i2: isize = 2;
+    let i3: u32 = 3;
+    let u4: usize = 4;
+    println!("{i1}");
+
+    // ponto flutuante
+    let d1 = 2.0;
+    let d2: f32 = 3.0;
+
+    // operações numéricas
+    let soma = 1 + 1;
+    let diferenca = 1 - 1;
+    let produto = 1 * 1;
+    let divisao = 1.0 / 2.0;
+    let resto = 43 % 5;
+    println!("{divisao}");
+
+    // booleano
+    let t = true;
+    let f: bool = false;
+
+    // caractere
+    let c = 'c';
+}
